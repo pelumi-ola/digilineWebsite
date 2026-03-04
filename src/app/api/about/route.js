5acbf4b13d0d5314998cff0c2f2f8fc13ce508b6
pelumi-ola <ola.e.pelumi@gmail.com>
2026-03-05 00:27:47 +0100

    first commit

--- src/app/api/about/route.js
+++ src/app/api/about/route.js
@@ -1,8 +1,8 @@
-import { fetchCollection } from "@/lib/api";
+// import { fetchCollection } from "@/lib/api";
 
-export async function GET() {
-  const data = await fetchCollection("about", {
-    filter: { slug: { _eq: "about" } },
-  });
-  return new Response(JSON.stringify(data || []), { status: 200 });
-}
+// export async function GET() {
+//   const data = await fetchCollection("about", {
+//     filter: { slug: { _eq: "about" } },
+//   });
+//   return new Response(JSON.stringify(data || []), { status: 200 });
+// }
